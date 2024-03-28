80d13b9b6a7d88113c2345a684a1ae4615e5d80f
morpole <morpole@gmail.com>
2024-03-28 18:23:25 +0000

    inital commit

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+// require('dotenv').config()
+
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.json({mssg: 'Welcome to the app'})
+});
+
+app.listen(4000, () => {
+    console.log('listening on port 4000')
+});
